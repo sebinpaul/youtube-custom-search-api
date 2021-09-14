34cd34caf9050262eb739e3ea4972d9408fe8582
ankurmaster <ankurjain@digitopper.com>
2021-09-14 21:59:56 +0530

    initial commit

---
+++ src/main/java/com/youtube/customsearchapi/YoutubeCustomSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.customsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCustomSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeCustomSearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youtube/customsearchapi/YoutubeCustomSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube.customsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCustomSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
