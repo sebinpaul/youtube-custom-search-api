a636280e110c011fe632d7089665e9ac60ad9c9c
sebinpaul <sebinpaul96@gmail.com>
2022-01-23 02:34:31 +0530

    Added code to fetch youtube video results every 10 seconds

--- src/main/java/com/youtube/customsearchapi/client/YoutubeSearchClient.java
+++ src/main/java/com/youtube/customsearchapi/client/YoutubeSearchClient.java
@@ -1,10 +1,46 @@
 package com.youtube.customsearchapi.client;
 
+import com.youtube.customsearchapi.dao.YoutubeVideoMetaDataDao;
+import com.youtube.customsearchapi.dto.YoutubeSearchResultDto;
+import com.youtube.customsearchapi.model.Items;
+import com.youtube.customsearchapi.model.VideoSnippet;
+import com.youtube.customsearchapi.model.YoutubeVideoMetaData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class YoutubeSearchClient {
 
-    private String URL = "https://www.googleapis.com/youtube/v3/search";
+    private final String BASE_URL = "https://www.googleapis.com/youtube/v3/search";
+    private final String API_KEY = "AIzaSyBg2koUNLBhlQtbhhj9pRl5M2b1vJMGouQ";
 
-    public String getYoutubeSearchURL(){
-        return URL;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private YoutubeVideoMetaDataDao youtubeVideoMetaDataDao;
+
+    @Scheduled(fixedRate = 10000)
+    public void fetchResultsFromYoutubeInBackground() {
+        String query = BASE_URL + "?key=" + API_KEY + "&&q=cricket" + "&&maxResults=25&&part=snippet&&publishedAfter=2019-09-16T14:30:06Z";
+        YoutubeSearchResultDto youtubeSearchResultDTO = restTemplate.getForObject(query, YoutubeSearchResultDto.class);
+        if (youtubeSearchResultDTO != null) {
+            List<Items> metaDataList = youtubeSearchResultDTO.getYoutubeVideoMetaData();
+            List<YoutubeVideoMetaData> youtubeVideoMetaDataList = new ArrayList<>();
+            for (Items items : metaDataList) {
+                VideoSnippet videoSnippet = items.getSnippet();
+                YoutubeVideoMetaData youtubeVideoMetaData = new YoutubeVideoMetaData(videoSnippet.getTitle(), videoSnippet.getDescription(), videoSnippet.getPublishTime(), videoSnippet.getThumbnails());
+                youtubeVideoMetaDataList.add(youtubeVideoMetaData);
+            }
+            youtubeVideoMetaDataDao.saveYoutubeVideoMetaData(youtubeVideoMetaDataList);
+        }
+
     }
+
+
 }
