41466e0d66e7893718c5d17878fc1f4596e881f7
sebinpaul <sebinpaul96@gmail.com>
2022-01-23 02:32:36 +0530

    Updated main class to enable scheduling

--- src/main/java/com/youtube/customsearchapi/YoutubeCustomSearchApiApplication.java
+++ src/main/java/com/youtube/customsearchapi/YoutubeCustomSearchApiApplication.java
@@ -1,24 +1,23 @@
 package com.youtube.customsearchapi;
 
-import com.youtube.customsearchapi.model.YoutubeDataDTO;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class YoutubeCustomSearchApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YoutubeCustomSearchApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeCustomSearchApiApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 
 }
